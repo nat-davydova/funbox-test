054026c352abe6ec551a7f61f0a9534ddd512326
nat-davydova <natti.davydova@gmail.com>
2020-08-20 16:25:00 +0300

    Pick functionality: pick data-attr changing onclick

--- funbox-test-js/src/scripts/modules/feedPack.js
+++ funbox-test-js/src/scripts/modules/feedPack.js
@@ -1,10 +1,21 @@
 const PACK_DISABLED_VAL = 'disabled'
+const PACK_PICKED_VAL = 'picked'
+const PACK_UNPICKED_VAL = ''
 
 export const pickFeedPack = pack => {
-
-  const packDisabledVal = pack.dataset.packDisabled;
+  // check if pack is disabled or not, if disabled - no picking allowed
+  const packDisabledVal = pack.dataset.packDisabled
 
   if (packDisabledVal === PACK_DISABLED_VAL) {
     return
+  }
+
+  // if pack is not disabled, picking functional
+  const packPickedVal = pack.dataset.packPicked
+
+  if (packPickedVal !== PACK_PICKED_VAL) {
+    pack.dataset.packPicked = PACK_PICKED_VAL
+  } else {
+    pack.dataset.packPicked = PACK_UNPICKED_VAL
   }
 }
