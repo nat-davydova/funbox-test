1200ef1d94941b7c3a52ed075e2d3c7519853198
nat-davydova <natti.davydova@gmail.com>
2020-08-20 16:17:45 +0300

    Pick functionality: testing if pack is disabled or not

--- funbox-test-js/src/scripts/modules/feedPack.js
+++ funbox-test-js/src/scripts/modules/feedPack.js
@@ -1,3 +1,10 @@
+const PACK_DISABLED_VAL = 'disabled'
+
 export const pickFeedPack = pack => {
 
+  const packDisabledVal = pack.dataset.packDisabled;
+
+  if (packDisabledVal === PACK_DISABLED_VAL) {
+    return
+  }
 }
