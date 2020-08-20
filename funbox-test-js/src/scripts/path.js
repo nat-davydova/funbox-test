1ce6476c0a0e7a89f1bc0c2ac862cd01c050ca64
nat-davydova <natti.davydova@gmail.com>
2020-08-20 16:05:21 +0300

    Pick functionality: setting basic event listener

---
+++ funbox-test-js/src/scripts/path.js
@@ -0,0 +1,6 @@
+export const PATH = {
+  feedPack: {
+    card: '.feed-pack__card',
+    orderBtn: '.feed-pack__order-btn'
+  }
+}
--- funbox-test-js/src/scripts/scripts.js
+++ funbox-test-js/src/scripts/scripts.js
@@ -1,1 +1,17 @@
+import { PATH } from './path'
 
+(function () {
+  'use strict'
+
+  // *** EVENT LISTENERS
+  document.body.addEventListener('click', e => {
+    const target = e.target;
+
+    if (
+      target.closest(PATH.feedPack.card) ||
+      target.closest(PATH.feedPack.orderBtn)
+    ) {
+      console.log('pick!')
+    }
+  })
+})()
