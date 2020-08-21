13c3b343f99a81a7286bb59356ea7ec9c4d14cff
nat-davydova <natti.davydova@gmail.com>
2020-08-21 17:59:35 +0300

    Layout: section - title styles

--- funbox-test-react/src/components/Section/SectionHeader/SectionHeader.js
+++ funbox-test-react/src/components/Section/SectionHeader/SectionHeader.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import styles from "./SectionHeader.module.scss";
+
 const sectionHeader = ({ title }) => (
-  <header>
-    <h1>{title}</h1>
+  <header className={styles.sectionHeader}>
+    <h1 className={styles.sectionTitle}>{title}</h1>
   </header>
 );
 
