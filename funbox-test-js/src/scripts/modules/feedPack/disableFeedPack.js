bd2615614ff660ac0d3b20fe8ea08741dbbf934f
nat-davydova <natti.davydova@gmail.com>
2020-08-21 13:45:29 +0300

    Disabling feature: disabling function completed

--- funbox-test-js/src/scripts/modules/feedPack/disableFeedPack.js
+++ funbox-test-js/src/scripts/modules/feedPack/disableFeedPack.js
@@ -3,6 +3,20 @@ import * as packConsts from './consts'
 
 import { PATH } from '../../path'
 
+const disablePack = pack => {
+  const packCTA = pack.querySelector(PATH.feedPack.ctaText)
+  const packPickedTxt = pack.querySelector(PATH.feedPack.pickedText)
+  const packDisabledTxt = pack.querySelector(PATH.feedPack.disabledText)
+
+  hideElem(packCTA)
+  hideElem(packPickedTxt)
+  showElem(packDisabledTxt)
+}
+
 export const disableFeedPack = () => {
-  console.log('disabling')
+  const packsToDisable = Array.from(
+    document.querySelectorAll(`${PATH.feedPack.pack}[data-pack-disabled = ${packConsts.PACK_DISABLED_VAL}]`)
+  )
+
+  packsToDisable.forEach(elem => disablePack(elem))
 }
--- funbox-test-js/src/scripts/path.js
+++ funbox-test-js/src/scripts/path.js
@@ -2,6 +2,7 @@ export const PATH = {
   feedPack: {
     card: '.feed-pack__card',
     ctaText: '.feed-pack__order-cta',
+    disabledText: '.feed-pack__disabled-info',
     pack: '.feed-pack',
     pickedText: '.feed-pack__picked-info',
     orderBtn: '.feed-pack__order-btn'
