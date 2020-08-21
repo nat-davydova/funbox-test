c6255bf9df81be35e42ac7695646b5f258b2f425
nat-davydova <natti.davydova@gmail.com>
2020-08-21 17:31:59 +0300

    Layout: section - basic component structure

---
+++ funbox-test-react/src/components/Section/Section.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const section = ({ children }) => <section>{children}</section>;
+
+export default section;
--- funbox-test-react/src/containers/FeedPacks/FeedPacks.js
+++ funbox-test-react/src/containers/FeedPacks/FeedPacks.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
+import Section from "components/Section/Section";
+
 class FeedPacks extends Component {
   state = {};
 
   render() {
-    return null;
+    return <Section />;
   }
 }
 
