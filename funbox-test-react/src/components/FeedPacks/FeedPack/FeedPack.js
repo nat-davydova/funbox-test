31a3e69f3a865cb6abcb3cd28c9d2ab59f491c68
nat-davydova <natti.davydova@gmail.com>
2020-08-21 18:24:57 +0300

    Layout: feedpacks - feedpack basics

---
+++ funbox-test-react/src/components/FeedPacks/FeedPack/FeedPack.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const feedPack = props => <article></article>;
+
+export default feedPack;
--- funbox-test-react/src/components/FeedPacks/FeedPacks.js
+++ funbox-test-react/src/components/FeedPacks/FeedPacks.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import FeedPack from "./FeedPack/FeedPack";
+
 import styles from "./FeedPacks.module.scss";
 
-const feedPacks = props => <div className={styles.feedPacks}></div>;
+const feedPacks = props => (
+  <div className={styles.feedPacks}>
+    <FeedPack />
+    <FeedPack />
+    <FeedPack />
+  </div>
+);
 
 export default feedPacks;
