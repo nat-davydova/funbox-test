a001639fb21b41c729d718a58a6321ada7c04c6d
nat-davydova <natti.davydova@gmail.com>
2020-08-21 17:39:34 +0300

    Layout: section - adding section content structure part

--- funbox-test-react/src/components/Section/Section.js
+++ funbox-test-react/src/components/Section/Section.js
@@ -6,7 +6,9 @@ import styles from "./Section.module.scss";
 
 const section = ({ children }) => (
   <section className={styles.section}>
-    <Container>{children}</Container>
+    <Container>
+      <div className="section__content">{children}</div>
+    </Container>
   </section>
 );
 
