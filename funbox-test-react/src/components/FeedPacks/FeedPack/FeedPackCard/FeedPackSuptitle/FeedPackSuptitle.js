4ad5797ce03d3b0199e5ca917a816a44596194a1
nat-davydova <natti.davydova@gmail.com>
2020-08-23 13:58:27 +0300

    Feed Pack: proptypes - suptitle and title component

--- funbox-test-react/src/components/FeedPacks/FeedPack/FeedPackCard/FeedPackSuptitle/FeedPackSuptitle.js
+++ funbox-test-react/src/components/FeedPacks/FeedPack/FeedPackCard/FeedPackSuptitle/FeedPackSuptitle.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import styles from "./FeedPackSuptitle.module.scss";
 
@@ -7,5 +8,12 @@ const feedPackSuptitle = ({ children }) => (
     <p>{children}</p>
   </div>
 );
+
+feedPackSuptitle.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.object.isRequired
+  ])
+};
 
 export default feedPackSuptitle;
--- funbox-test-react/src/components/FeedPacks/FeedPack/FeedPackCard/FeedPackTitle/FeedPackTitle.js
+++ funbox-test-react/src/components/FeedPacks/FeedPack/FeedPackCard/FeedPackTitle/FeedPackTitle.js
@@ -1,9 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import styles from "./FeedPackTitle.module.scss";
 
 const feedPackTitle = ({ children }) => (
   <h2 className={styles.title}>{children}</h2>
 );
+
+feedPackTitle.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.object.isRequired
+  ])
+};
 
 export default feedPackTitle;
