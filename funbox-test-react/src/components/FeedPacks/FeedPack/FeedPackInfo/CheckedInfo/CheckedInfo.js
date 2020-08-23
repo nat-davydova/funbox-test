fa5a7ad9d26d6b90e70baf47c0e9136e5c53eaad
nat-davydova <natti.davydova@gmail.com>
2020-08-23 16:14:14 +0300

    Feed Pack: pack info - disabled info proptypes and checked info proptypes

--- funbox-test-react/src/components/FeedPacks/FeedPack/FeedPackInfo/CheckedInfo/CheckedInfo.js
+++ funbox-test-react/src/components/FeedPacks/FeedPack/FeedPackInfo/CheckedInfo/CheckedInfo.js
@@ -1,5 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const checkedInfo = ({ descr }) => <p>{descr}</p>;
+
+checkedInfo.propTypes = {
+  descr: PropTypes.string.isRequired
+};
 
 export default checkedInfo;
--- funbox-test-react/src/components/FeedPacks/FeedPack/FeedPackInfo/DisabledInfo/DisabledInfo.js
+++ funbox-test-react/src/components/FeedPacks/FeedPack/FeedPackInfo/DisabledInfo/DisabledInfo.js
@@ -1,9 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import styles from "./DisabledInfo.module.scss";
 
 const disabledInfo = ({ type }) => (
   <p className={styles.disabled}>Печалька, {type} закончился</p>
 );
+
+disabledInfo.propTypes = {
+  type: PropTypes.string.isRequired
+};
 
 export default disabledInfo;
