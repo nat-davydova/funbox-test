a908ec72f69bfe5b410242d56e68b2e988fc48bc
nat-davydova <natti.davydova@gmail.com>
2020-08-23 13:24:02 +0300

    Feed Pack: img - styles

--- funbox-test-react/src/components/FeedPacks/FeedPack/FeedPackCard/FeedPackImg/FeedPackImg.js
+++ funbox-test-react/src/components/FeedPacks/FeedPack/FeedPackCard/FeedPackImg/FeedPackImg.js
@@ -2,10 +2,20 @@ import React from "react";
 
 import Img from "components/Misc/Img/Img";
 
+import styles from "./FeedPackImg.module.scss";
+
 const feedPackImg = ({ imgInfo }) => {
   const { img, alt } = imgInfo;
 
-  return <Img img={img} alt={alt} />;
+  return (
+    <Img
+      classes={{
+        root: styles.img
+      }}
+      img={img}
+      alt={alt}
+    />
+  );
 };
 
 export default feedPackImg;
