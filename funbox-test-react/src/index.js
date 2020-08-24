77754685bb69d843d57bf1671a64033613d24034
nat-davydova <natti.davydova@gmail.com>
2020-08-24 09:45:26 +0300

    Redux Preps: creating store and adding redux devtools

--- funbox-test-react/src/index.js
+++ funbox-test-react/src/index.js
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import App from "./App";
 
+import reducer from "./store/reducer";
+
 import "./index.scss";
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
