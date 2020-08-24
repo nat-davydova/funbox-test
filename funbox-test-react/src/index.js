95071f4afe41d670d284c4901d4f48db504cc67d
nat-davydova <natti.davydova@gmail.com>
2020-08-24 09:52:35 +0300

    Rendering Packs: minor reducer folder structure tweaks, adding config file

--- funbox-test-react/src/index.js
+++ funbox-test-react/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 
 import App from "./App";
 
-import reducer from "./store/reducer";
+import reducer from "./store/feedPackReducer/reducer";
 
 import "./index.scss";
 
