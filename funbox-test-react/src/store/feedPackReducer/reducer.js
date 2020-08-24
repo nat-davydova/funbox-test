fe1cf7dbeab6b19920bc380256f8af89d15659f3
nat-davydova <natti.davydova@gmail.com>
2020-08-24 10:50:08 +0300

    Picking elem: basic reducer connected to action

--- funbox-test-react/src/store/feedPackReducer/reducer.js
+++ funbox-test-react/src/store/feedPackReducer/reducer.js
@@ -1,11 +1,19 @@
 import { feedPacksConfig } from "./config";
 
+import * as actionTypes from "store/actions/actions";
+
 const initState = {
   feedPacks: [...feedPacksConfig]
 };
 
 const reducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case actionTypes.CHANGE_PICKING_STATE:
+      console.log("click!");
+      return state;
+    default:
+      return state;
+  }
 };
 
 export default reducer;
